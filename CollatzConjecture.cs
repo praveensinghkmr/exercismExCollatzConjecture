fa9bb5a87a8a3113d196645dc05b7da4bbbc8a44
praveensinghkmr <praveensingh.kmr@gmail.com>
2018-10-01 16:15:30 +0530

    Implemented logic for values greater than zero

--- CollatzConjecture.cs
+++ CollatzConjecture.cs
@@ -4,6 +4,20 @@ public static class CollatzConjecture
 {
     public static int Steps(int number)
     {
-        throw new NotImplementedException("You need to implement this function");
+        int steps = 0;
+        int num = number;
+        while (num > 1)
+        {
+            if (num % 2 == 0)
+            {
+                num = num / 2;
+            }
+            else
+            {
+                num = num * 3 + 1;
+            }
+            steps++;
+        }
+        return steps;
     }
 }
